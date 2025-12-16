6d0c4cc6ccbf69cd34dc96539c25d6c76ccd1797
nandinireddi5992 <nandinireddi5992@gmail.com>
2025-12-16 22:54:16 +0530

    Add multi-component page with Navbar, Content, Footer

---
+++ MultiComponent/src/App.jsx
@@ -0,0 +1,11 @@
+import MainPage from "./MainPage";
+
+function App() {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ MultiComponent/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div style={{ fontSize: "12px", color: "gray", marginTop: "20px", textAlign: "center" }}>
+      This is Footer
+    </div>
+  );
+}
+
+export default Footer;
+
