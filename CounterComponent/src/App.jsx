52e01289515c5c42972a988ed806c966c304fdaf
nandinireddi5992 <nandinireddi5992@gmail.com>
2025-12-17 22:51:10 +0530

    Build counter component using useState

---
+++ CounterComponent/src/App.jsx
@@ -0,0 +1,12 @@
+import Counter from "C:\Users\Bobby\OneDrive\Desktop\ReactAssignmentsRepo\CounterComponent\src\components\Counter";
+
+function App() {
+  return (
+    <div>
+      <h1>My Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
